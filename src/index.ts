4a4f496783d1a8cb73a0b399b0ba88de2d2126ed
Romain🔥 <romaintibo6@gmail.com>
2024-12-06 14:32:52 +0100

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
+import { node } from "@elysiajs/node";
 import { Elysia } from "elysia";
 
-const app = new Elysia()
+const app = new Elysia({ adapter: node() })
 	.get("/", () => "OK")
 	.get("/api/hello", () => ({ message: "Hello" }));
-
 export const handler = (req: Request) => app.handle(req);
 export default app;
